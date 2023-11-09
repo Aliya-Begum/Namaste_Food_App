663403288ca6cca64fd197aa0afdc4aa0593534f
Aliya Begum <aliyaansari0604@gmail.com>
2023-11-09 19:22:26 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,3 @@
+const heading = React.createElement("h1", {id: "heading"}, "Creating React element using CDN link");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
